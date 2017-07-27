4d9a96b9a05861e5bb1b88699cf88ad8d1b47a3a
Akos Veres <veres@akos.me>
2017-07-27 20:30:37 +0100

    Rewritten code to use cache instead of a in-the-middle db
    * Huge thanks to #go-nuts on Freenode, especially "awly"

--- xtradio-api.go
+++ xtradio-api.go
@@ -8,7 +8,7 @@ import (
     "github.com/gorilla/mux"
     _ "github.com/go-sql-driver/mysql"
     "database/sql"
-    boltDB "github.com/boltdb/bolt"
+    "sync"
 )
 
 type Song struct {
@@ -24,14 +24,21 @@ type Song struct {
     Url string `json:"url"`
     }
 
-type Songs []Song
-    
+type cache struct {
+	sync.RWMutex
+	song Song
+}
+
+type songsHandler struct {
+    c *cache
+}
+
 func homePage(w http.ResponseWriter, r *http.Request){
     fmt.Fprintf(w, "XTRadio API.")
     fmt.Println("Endpoint Hit: homePage")
 }
 
-func readPost(w http.ResponseWriter, r *http.Request){
+func (h songsHandler) readPost(w http.ResponseWriter, r *http.Request){
     vars := mux.Vars(r)
     var song Song
     // Open and connect do DB
@@ -45,74 +52,38 @@ func readPost(w http.ResponseWriter, r *http.Request){
     err = query.Scan(&song.Artist, &song.Title, &song.Album, &song.Length, &song.Share, &song.Url, &song.Image)
     checkErr(err)
 
-    saveData("artist", song.Artist)
-    saveData("title", song.Title)
-    saveData("album", song.Album)
-    saveData("length", song.Length)
-    saveData("share", song.Share)
-    saveData("url", song.Url)
-    saveData("image", song.Image)
-
     db.Close()
+
+    h.c.Lock()
+    defer h.c.Unlock()
+    h.c.song = song
 
     fmt.Fprintf(w, "Song: %v", vars["file"])
     fmt.Println("readPost hit:", vars["file"], song.Artist, song.Title)
 }
 
-func returnSongs(w http.ResponseWriter, r *http.Request){
+func (h songsHandler) returnSongs(w http.ResponseWriter, r *http.Request){
+    h.c.RLock()
+    defer h.c.RUnlock()
+
     fmt.Println("Endpoint Hit: returnApi")
     w.Header().Set("Access-Control-Allow-Origin", "*") 
 
-    bolt, err := boltDB.Open("my.db", 0600, nil)
-    checkErr(err)
-
-    var song []byte
-
-    bolt.View(func(tx *boltDB.Tx) error {
-	b := tx.Bucket([]byte("songlist"))
-	b.ForEach(func(k, v []byte) error {
-		fmt.Printf("key=%s, value=%s\n", k, v)
-                return nil
-	})
-	v := b.Get([]byte("artist"))
-	fmt.Printf("The answer is: %s\n", v)
-	return nil
-    })
-    defer bolt.Close()
-    json.NewEncoder(w).Encode(song)
+    json.NewEncoder(w).Encode(h.c.song)
 }
 
 func publishApi() {
     apiRouter := mux.NewRouter().StrictSlash(true)
     apiRouter.HandleFunc("/", homePage)
-    apiRouter.HandleFunc("/api", returnSongs)
-    apiRouter.HandleFunc("/post/song", readPost).
+    sh := songsHandler{c: &cache{}}
+    apiRouter.HandleFunc("/api", sh.returnSongs)
+    apiRouter.HandleFunc("/post/song", sh.readPost).
               Name("putsong").
               Queries("file", "{file}")
 
     log.Fatal(http.ListenAndServe(":10000", apiRouter))
 }
 
-func saveData(key, value  string) error {
-    bolt, err := boltDB.Open("my.db", 0600, nil)
-    checkErr(err)
-
-    bolt.Update(func(tx *boltDB.Tx) error {
-	b, err := tx.CreateBucketIfNotExists([]byte("songlist"))
-        checkErr(err)
-
-	err2 := b.Put([]byte(key), []byte(value))
-	if err2 != nil {
-		return fmt.Errorf("create value: %s", err2)
-	}
-        fmt.Println("Data save: ", key, value)
-	return nil
-    })
-
-    defer bolt.Close()
-
-    return nil
-}
 
 func checkErr(err error) {
     if err != nil {
