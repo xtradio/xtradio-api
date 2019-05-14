da6c8832526ddce9ac81dbd63bbc6d3f22d5dc21
Akos Veres <averes@equalexperts.com>
2019-05-14 21:07:48 +0200

    Output list of all the songs in the db

--- xtradio-api.go
+++ xtradio-api.go
@@ -29,6 +29,18 @@ type Song struct {
 	URL       string `json:"url"`
 }
 
+type SongDetails struct {
+	Id     int64  `json:"id"`
+	Title  string `json:"song"`
+	Artist string `json:"artist"`
+	Show   string `json:"show"`
+	Image  string `json:"image"`
+	Album  string `json:"album"`
+	Length string `json:"length"`
+	Share  string `json:"share"`
+	URL    string `json:"url"`
+}
+
 // Duration of the song
 type Duration struct {
 	Finished time.Time
@@ -259,6 +271,59 @@ func (h songsHandler) nowplaying(w http.ResponseWriter, r *http.Request) {
 	// Output json
 	json.NewEncoder(w).Encode(data)
 	log.Println(r.RemoteAddr, r.Method, r.URL)
+}
+
+func songList(w http.ResponseWriter, r *http.Request) {
+	var v struct {
+		Data []SongDetails `json:"data"`
+	}
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8", os.Getenv("MYSQL_USERNAME"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_DATABASE"))
+	// Open and connect do DB
+	db, err := sql.Open("mysql", connection)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		fmt.Println("Ping database failed.", err)
+		return
+	}
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		fmt.Println("Ping database failed.", err)
+		return
+	}
+
+	// Fetch details for the track
+	rows, err := db.Query("SELECT id, artist, title, album, lenght, share, url, image FROM details")
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		fmt.Println("Fetching rows failed.", err)
+		return
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var s SongDetails
+
+		err := rows.Scan(&s.Id, &s.Artist, &s.Title, &s.Album, &s.Length, &s.Share, &s.URL, &s.Image)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			fmt.Println("Fetching item failed.", err)
+			return
+		}
+		v.Data = append(v.Data, s)
+	}
+	fmt.Println(v)
+	p, err := json.Marshal(v)
+	if err != nil {
+		fmt.Println("Error on defining json", err)
+		return
+	}
+	fmt.Fprintf(w, string(p))
+
 }
 
 func publishAPI() {
