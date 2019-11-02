280f22556376d309b5c9a9a664db8c761c15137a
Akos Veres <averes@equalexperts.com>
2019-11-02 21:02:36 +0100

    New getEnv func and adding logging
    * Proper error returns

--- querysql.go
+++ querysql.go
@@ -3,34 +3,68 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"os"
 )
 
-func querysql(query string) *sql.Rows {
+func getEnv(envKey string) (envValue string, err error) {
 
-	connection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8", os.Getenv("MYSQL_USERNAME"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_DATABASE"))
+	envValue, ok := os.LookupEnv(envKey)
+	if ok != true {
+		err = fmt.Errorf("please set %s environment variable", envKey)
+		return
+	}
+
+	return envValue, nil
+}
+
+func querysql(query string) (*sql.Rows, error) {
+
+	username, err := getEnv("MYSQL_USERNAME")
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	password, err := getEnv("MYSQL_PASSWORD")
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	host, err := getEnv("MYSQL_HOST")
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	database, err := getEnv("MYSQL_DATABASE")
+	if err != nil {
+		log.Print(err)
+		return nil, err
+	}
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8", username, password, host, database)
+
 	// Open and connect do DB
 	db, err := sql.Open("mysql", connection)
 	if err != nil {
-		// http.Error(w, err.Error(), 500)
-		fmt.Println("Ping database failed.", err)
-		// return "Open database failed"
+		log.Printf("Opening db connection failed: %s", err)
+		return nil, err
 	}
 
 	// Open doesn't open a connection. Validate DSN data:
 	err = db.Ping()
 	if err != nil {
-		// http.Error(w, err.Error(), 500)
-		fmt.Println("Ping database failed.", err)
-		// return "Ping database failed"
+		log.Printf("Ping database failed: %s", err)
+		return nil, err
 	}
 
 	rows, err := db.Query(query)
 	if err != nil {
-		// http.Error(w, err.Error(), 500)
-		fmt.Println("Fetching rows failed.", err)
-		// return "Fetching rows failed"
+		fmt.Printf("Fetching rows failed: %s", err)
+		return nil, err
 	}
 
-	return rows
+	return rows, nil
 }
